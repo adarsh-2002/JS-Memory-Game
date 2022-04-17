04a8bacffaf36bcb62bc5ce55bf4eeed7a615ef4
Adarsh Sankar <61370366+adarsh-2002@users.noreply.github.com>
2022-04-17 19:29:17 +0530

    Added timer and rounds

--- scripts.js
+++ scripts.js
@@ -1,12 +1,47 @@
 const cards = document.querySelectorAll(".mem-card");
-score = 0;
-flips = 0;
 const scoreEl = document.getElementById("score");
 const flipEl = document.getElementById("flips");
-round = 1;
+const pointEl = document.getElementById("points");
+const timerEl = document.getElementById("timer");
+const roundEl = document.getElementById("round");
+const levelTimerEl = document.getElementById("level-timer");
+
 let hasFlipped = false;
 let firstCard, secondCard;
 let lockBoard = false;
+let timeLeft = 300;
+
+cardsMatched = 0;
+flips = 0;
+points = 0;
+round = 1;
+timer = 300;
+timers = [300, 180, 60];
+var theActualTimer;
+
+function gameStart(round = 1) {
+  theActualTimer = setInterval(updateTimer, 1000);
+  document.getElementById("start-msg").style.display = "none";
+  cards.forEach((card) => card.addEventListener("click", flipCard));
+  cards.forEach((card) => {
+    card.classList.remove("flip");
+    card.style.visibility = "visible";
+  });
+  cardsMatched = 0;
+  flips = 0;
+  points = 0;
+  round = round;
+  timer = timers[round - 1];
+  timeLeft = timer;
+  updateBoard();
+}
+
+// (function shuffle() {
+//     cards.forEach(card => {
+//         let randomPos = Math.floor(Math.random()*12);
+//         card.style.order = randomPos;
+//     });
+// })();
 
 function flipCard() {
   if (lockBoard) return;
@@ -22,7 +57,7 @@ function flipCard() {
   //second click
   //increment no. of flip attempts
   flips += 1;
-  flipEl.innerText = flips;
+  calcPoints();
   hasFlipped = false;
   secondCard = this;
   //check match
@@ -34,9 +69,10 @@ function checkMatch() {
     //cards match
     disableCards();
     //increment score
-    score += 1;
-    if (score == 12) gameOver();
-    scoreEl.innerText = score;
+    cardsMatched += 2;
+    calcPoints();
+    if (cardsMatched == 24) gameOver();
+    updateBoard();
   } else {
     resetFlip();
   }
@@ -67,17 +103,44 @@ function resetBoard() {
   [firstCard, secondCard] = [null, null];
 }
 
-(function shuffle() {
-    cards.forEach(card => {
-        let randomPos = Math.floor(Math.random()*12);
-        card.style.order = randomPos;
-    });
-})();
-
 function gameOver() {
-    setTimeout(() => {
-        window.alert("Game over");
-    }, 500);
+  cards.forEach((card) => card.removeEventListener("click", flipCard));
+  setTimeout(() => {
+    if (cardsMatched == 24) {
+      if (round === 3) {
+        window.alert("You have cleared all 3 rounds!! Press OK to restart.");
+        location.reload();
+        return;
+      }
+      window.alert("Level Cleared!! Press OK to start next round.");
+      round += 1;
+      timer = timers[round - 1];
+      gameStart(round);
+      return;
+    } else {
+      window.alert("Level failed!! Press OK to restart.");
+      location.reload();
+    }
+  }, 500);
 }
 
-cards.forEach((card) => card.addEventListener("click", flipCard));
+function calcPoints() {
+  points = (cardsMatched / flips) * 50;
+  updateBoard();
+}
+
+function updateBoard() {
+  scoreEl.innerText = cardsMatched;
+  flipEl.innerText = flips;
+  pointEl.innerText = Math.floor(points);
+  roundEl.innerText = round;
+  levelTimerEl.innerText = timers[round - 1];
+}
+
+function updateTimer() {
+  timeLeft = timeLeft - 1;
+  if (timeLeft >= 0) timerEl.innerText = timeLeft;
+  else {
+    gameOver();
+  }
+}
